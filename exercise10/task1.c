25064b960a37171ade829a9c8abe9d227f4fc751
christoph <christoph.geyer1996@student.uibk.ac.at>
2022-05-27 13:18:44 +0200

    task1 my_malloc_init should work

--- exercise10/task1.c
+++ exercise10/task1.c
@@ -3,6 +3,8 @@
 #include <sys/mman.h>
 #include <string.h>
 
+#include "allocator_tests.h"
+
 #define BLOCK_SIZE 1024
 
 void* my_malloc(size_t size);
@@ -11,50 +13,78 @@ void my_allocator_init(size_t size);
 void my_allocator_destroy(void);
 
 typedef struct Node {
-    char *block;
+    char block[BLOCK_SIZE];
     struct Node* next;
 } node_t;
 
 
 node_t *head;
+node_t *current;
+
+node_t *begin;
 
 int main(void) {
-    my_allocator_init(10);
-    printf("%p\n", my_malloc(BLOCK_SIZE));
-
+    test_free_list_allocator();
+    //my_allocator_init(10000);
     return EXIT_SUCCESS;
 }
 
-void * my_malloc(size_t size) {
-    node_t *current_node = head;
-    while(current_node != NULL) {
-        if (strcmp(current_node->block, "free") == 0) {
-            current_node->block = "allocd";
-           
-            return &current_node;
-        } else {
-            current_node = current_node->next;
-        }
+void *my_malloc(size_t size) {
+    if (size >= BLOCK_SIZE) {
+        return NULL;
     }
+
+   if (current != NULL) { 
+       node_t * my_malloc_address = current;
+       current = current->next;
+        return my_malloc_address;
+   }
+   return NULL;
 }
 
 void my_free(void* ptr) {
+    current = ptr;
+
 }
 
 void my_allocator_init(size_t size) {
+    begin = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1 ,0);
+    
+    head = begin;
+    current = begin;
+    head->next = current;
 
-    mmap(NULL, size * BLOCK_SIZE, PROT_NONE, MAP_PRIVATE, 0 ,0);
-   
-    node_t node_arr[size];
-    for (size_t i = 0; i < size; i++) {
-        node_arr[i].block = "free";
-        if ((i + 1) == size) {
-            node_arr[i].next = NULL;
+    size_t i = 0;
+    /* while(1) {
+        i += BLOCK_SIZE;
+        if (i > size) {
+            break;
         }
-        node_arr[i].next = &node_arr[i + 1];
+        node_t next_node; 
+        
+        current->next = &next_node;
+        current = &next_node;
+    } */
+    
+    while(1) {
+        i += sizeof(node_t);
+        if (i > size) {
+            break;
+        }
+        node_t *next_node = (node_t *) begin++;
+        current->next = next_node;
+        current = next_node;
+
     } 
+
+    current->next = NULL;
+    current = head;
+
 }
 
 void my_allocator_destroy(void) {
-    //munmap();
+    if (munmap(begin, BLOCK_SIZE) != 0) {
+        perror("munmap\n");
+        exit(EXIT_FAILURE);
+    } 
 }
