713394a36065dcd21846d87d6f96151dcee09513
christoph <christoph.geyer1996@student.uibk.ac.at>
2022-06-02 15:47:48 +0200

    task3 done

---
+++ exercise11/task3/malloc_spy.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h> //for write()
+#define __USE_GNU // for RTLD_NEXT
+#include <dlfcn.h> //for dlopen and dlerror
+
+
+void print_number(size_t number) {
+	if(number > 9) {
+		print_number(number / 10);
+	}
+	const char digit = '0' + number % 10;
+	write(STDOUT_FILENO, &digit, 1);
+}
+
+void * malloc(size_t size) {
+    print_number(size);
+    write(STDOUT_FILENO, "\t", 1);
+    
+    void *(*original_malloc)(size_t);
+    
+        *(void **) (&original_malloc) = dlsym(RTLD_NEXT, "malloc");
+        char *error = dlerror();
+        if (error != NULL) {
+            fprintf(stderr, "%s\n", error);
+            exit(EXIT_FAILURE);
+        }
+
+    return (*original_malloc)(size);
+}
+
