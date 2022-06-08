3b1642a59eb496f472a01c0c2412139271bdeb0c
christoph <christoph.geyer1996@student.uibk.ac.at>
2022-06-08 20:53:35 +0200

    added small details and more comments

--- exercise12/restaurant.c
+++ exercise12/restaurant.c
@@ -11,14 +11,15 @@
 myqueue queue_counter;
 myqueue queue_order;
 
-//pthreads mutexes and Condition variables, a pair for each queue
+//pthreads mutexes and condition variables, a pair for each queue
 pthread_cond_t cond_variable;
 pthread_cond_t counter_cond;
 pthread_mutex_t mutex_counter;
 pthread_mutex_t mutex_order;
 
-//end-marker for cook thread, should jump out of the loop (what is not working yet)
+//end-marker for cook thread, exits the method)
 bool exit_cooks = false;
+//boolean for the notification addon of the exam
 bool notifications_enabled = false;
 
 //pthread methods for cooks and guests
@@ -31,13 +32,13 @@ typedef struct guest_info {
     int guest_nr; 
     int ordering_nr;
     uint64_t waiting_time_ms;
-    pthread_cond_t guest_cond_var;
+    pthread_cond_t guest_cond_var; //only needed for notifications
 } guest_info;
 
 //a guest struct to get information into the pthread method
 typedef struct cook_info {
     int cook_nr;
-    guest_info *guests_info;
+    guest_info *guests_info; //ptr to every guest_info
 } cook_info;
 
 int main(int argc, char* argv[]) {
@@ -64,7 +65,7 @@ int main(int argc, char* argv[]) {
     myqueue_init(&queue_counter);
     myqueue_init(&queue_order);
 
-    //initializising for the Mutexes and Cond_variables
+    //initializising for the mutexes and cond_variables
     //if the method don't return 0 there is an error
     if(pthread_mutex_init(&mutex_counter, NULL) != 0) {
         printf("Mutex init failed\n");
@@ -89,7 +90,7 @@ int main(int argc, char* argv[]) {
     
 
     // getting the right info stored and creating the pthreads with the guests method
-
+    // if notifications are enabled the condition_var is also initialized and the other method is used
     if (notifications_enabled == true) {
 
         for (int i = 0; i < number_of_guests; i++) {
@@ -121,20 +122,18 @@ int main(int argc, char* argv[]) {
     }
 
 
-
     uint64_t overall_time = 0;
-    // loop to wait until all guests pthreads are finished (got their meal)
+    // loop to wait until all guests pthreads are finished (got their meal) and to sum up the overall_time
     for (int i = 0; i < number_of_guests; i++) {
         pthread_join(guests[i], NULL);
         overall_time += guests_info[i].waiting_time_ms; 
-        //printf("%d guests are back\n", i);
 
     }
 
     //setting the exit variable to true to stop all cooks from producuing more meals
     exit_cooks = true;
     
-    //sending a signal to get the cook_threads over the cond_var_wait
+    //sending a signal to get the cook_threads over the cond_var_wait and therefor exiting the thread
     for (int i = 0; i < number_of_cooks; i++) {
         pthread_cond_signal(&cond_variable);
     }
@@ -145,7 +144,7 @@ int main(int argc, char* argv[]) {
     }
 
     //last message before exiting. The info should be stored in the guest info and the calculate the average 
-    //didn't manage to finish it in time.
+    //didn't manage to finish it in time. But now it works 
     printf("All guests have been served with an average wait time for %ld ms\n", overall_time / number_of_guests);
 
     //destroy the mutexes that there are no memory leaks available
@@ -155,10 +154,11 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
-
+//method if notifications are enabled for guests
 void *notification_and_chill(void *ptr) {
     guest_info* info = (guest_info *) ptr;
 
+    //the order (number) will be stored in the order_queue;
     pthread_mutex_lock(&mutex_order);
     myqueue_push(&queue_order, info->ordering_nr);
     pthread_cond_signal(&cond_variable);
@@ -171,17 +171,16 @@ void *notification_and_chill(void *ptr) {
     printf("Guest %d has made meal order %d\n",info->guest_nr, info->ordering_nr);
     uint64_t start_ms = ts.tv_sec * 1e3 + ts.tv_nsec / 1e6;
 
-
-    pthread_cond_wait(&info->guest_cond_var, &mutex_counter);
+    // the methods now waits until the right number is in the counter_queue and the cond_var gets a signal 
+    pthread_cond_wait(&info->guest_cond_var, &mutex_counter); 
     
-    //pthread_mutex_lock(&mutex_counter);
     myqueue_pop(&queue_counter);
 
     clock_gettime(CLOCK_MONOTONIC, &ts);
     uint64_t end_ms = ts.tv_sec * 1e3 + ts.tv_nsec / 1e6;
     info->waiting_time_ms = end_ms - start_ms;
     printf("Guest %d has picked up order %d after %ld ms\n", info->guest_nr, info->ordering_nr, info->waiting_time_ms);
-    pthread_cond_signal(&counter_cond);
+    pthread_cond_signal(&counter_cond); // sends signal that the counter is free again
     pthread_mutex_unlock(&mutex_counter);
 
 
@@ -192,6 +191,7 @@ void *notification_and_chill(void *ptr) {
 void *order_and_wait_impatiently(void *ptr) {
     guest_info* info = (guest_info *) ptr;
 
+    //the order (number) will be stored in the order_queue;
     pthread_mutex_lock(&mutex_order);
     myqueue_push(&queue_order, info->ordering_nr);
     pthread_cond_signal(&cond_variable);
@@ -205,11 +205,13 @@ void *order_and_wait_impatiently(void *ptr) {
     uint64_t start_ms = ts.tv_sec * 1e3 + ts.tv_nsec / 1e6;
 
     int order_nr;
+    //here the method simulates the walk to the counter and the check if the real meal is allreade at the counter
     while(1) {
         usleep(100 * 1000);
         pthread_mutex_lock(&mutex_counter);
         if (! myqueue_is_empty(&queue_counter)) {
             usleep(100 * 1000);
+            // the meal need to be popped to check if the order_nr is right
             order_nr = myqueue_pop(&queue_counter);
             if (info->ordering_nr == order_nr) {
                 clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -220,6 +222,7 @@ void *order_and_wait_impatiently(void *ptr) {
                 pthread_mutex_unlock(&mutex_counter);
                 return NULL;
             } else {
+                //otherwise it need to get back into the queue
                 myqueue_push(&queue_counter, order_nr);
             }
         }
@@ -237,7 +240,9 @@ void *cook_meal(void *ptr) {
 
         pthread_mutex_lock(&mutex_order);
         while(myqueue_is_empty(&queue_order)) {
+            //waits until the ordering queue is not empy
             pthread_cond_wait(&cond_variable, &mutex_order);
+            //close condition
             if (exit_cooks == true) {
                 pthread_mutex_unlock(&mutex_order);
                 return NULL;
@@ -247,13 +252,17 @@ void *cook_meal(void *ptr) {
         order_nr = myqueue_pop(&queue_order);
         pthread_mutex_unlock(&mutex_order);
         printf("Cook %d is preparing order %d\n", info->cook_nr, order_nr);
+        srand(time(NULL)); //to genereate random numbers
         usleep((rand() % 5 + 1) * 100 * 1000); //for a number between 100 and 500 ms
         printf("Cook %d has finished order %d\n", info->cook_nr, order_nr);
         pthread_mutex_lock(&mutex_counter);
         while(! myqueue_is_empty(&queue_counter)) {
+            //waits until the counter (-queue) is empty to put the next meal on the table
             pthread_cond_wait(&counter_cond, &mutex_counter);
         }
         myqueue_push(&queue_counter, order_nr);
+        
+        // with notifications a signal will be send to the right guest condition variable
         if (notifications_enabled == true) {
             pthread_cond_signal(&info->guests_info[order_nr].guest_cond_var);
         }
